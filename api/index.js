a249517e4764e3f14decce4629b3dba0e3062c66
Ashvin <68969607+betheashvin@users.noreply.github.com>
2026-01-28 23:54:00 +0500

    Fixed ESM import error in api bridge

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
-import app from "../server/server.js";
-
-export default app;
+// api/index.js
+module.exports = async (req, res) => {
+    // 1. Dynamic import fixes the "ERR_REQUIRE_ESM" error
+    const { default: app } = await import('../server/server.js');
+    
+    // 2. Pass the request/response to your Express app
+    app(req, res);
+};
